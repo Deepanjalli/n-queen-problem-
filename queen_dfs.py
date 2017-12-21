4672baca04a87bb0f9af7e570f5d677f2d092baf
Deepanjalli <34731510+Deepanjalli@users.noreply.github.com>
2017-12-21 20:51:55 +0530

    Update queen_dfs.py

--- queen_dfs.py
+++ queen_dfs.py
@@ -31,21 +31,21 @@ def searchCol(col):
    numQueens+=1;                                             
    searchCol(col+1);                   ########repeat recursively for other columns
    board[i,col] =0;                    
-   for j in range(N):                    ########cells which do not have same column as the previous queen increase their value by 1
+   for j in range(N):                    ########Backtracking
     if(j != col):
      board[i,j]+=1;
-    if(j !=i):                             ########cells which do not have same row as the previous queen increase their value by 1
+    if(j !=i):                             
      board[j,col]+=1;
    for j in range(1,N):
     if ((i-j) >=0) and ((col -j) >=0):
      board[i-j,col-j]+=1;
-    if ((i+j) <N) and ((col +j) <N):        ######cells which are not diagonal to the previous queen increase their value by 1
+    if ((i+j) <N) and ((col +j) <N):        
      board[i+j,col+j]+=1;
     if ((i-j) >=0) and ((col +j) <N):
      board[i-j,col+j]+=1;
     if ((i+j) <N) and ((col -j) >=0):
      board[i+j,col-j]+=1;
-   numQueens-=1;                      ##########if no solution found then replace the queen and backtrack
+   numQueens-=1;                      k
 
 ###searchCol(0);
 ##print sols;
